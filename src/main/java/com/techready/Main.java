7893d75b11d58d347642da0efe584b9a1cd2703e
Scarlett AT <tsuki.scarlett@gmail.com>
2025-11-04 17:51:05 -0600

    feat: added CRUD endpoints

--- src/main/java/com/techready/Main.java
+++ src/main/java/com/techready/Main.java
@@ -1,5 +1,8 @@
 package com.techready;
 
+import com.techready.user.User;
+import com.techready.user.UserService;
+
 import static spark.Spark.*;
 import com.google.gson.Gson;
 
@@ -7,10 +10,77 @@ public class Main {
     public static void main(String[] args) {
         port(4567);
         Gson gson = new Gson();
+        UserService userService = new UserService();
 
-        get("/hello", (req, res) -> {
+
+//      Routes
+//          Retrieve all Users
+        get("/users", (req, res) ->{
             res.type("application/json");
-            return gson.toJson("Server running correctly!");
+            return gson.toJson(userService.getAllUsers());
+        });
+
+        // Retrieve user by ID
+        get("/users/:id", (req, res)->{
+            res.type("application/json");
+            int id = Integer.parseInt(req.params(":id"));
+            User user = userService.getUserById(id);
+
+            if(user == null){
+                res.status(404);
+                return gson.toJson("USER NOT FOUND");
+            }
+            return gson.toJson(user);
+        });
+
+        // POST -> add a new user
+        post("/users/:id", (req, res)->{
+            res.type( "application/json");
+            int id = Integer.parseInt(req.params(":id"));
+            User newUser = gson.fromJson(req.body(), User.class);
+
+            newUser.setId(id);
+            userService.addUser(newUser);
+
+            res.status(201);
+
+
+            return gson.toJson("USER ADDED!");
+        });
+
+
+//      PUT -> Edit an user
+        put("/users/:id", (req, res)->{
+            res.type( "application/json");
+            int id = Integer.parseInt(req.params(":id"));
+
+            User updateUser = gson.fromJson(req.body(), User.class);
+
+            updateUser.setId(id);
+            userService.updateUser(id, updateUser);
+
+            return gson.toJson("USER UPDATED!");
+
+        });
+
+        // OPTIONS -> Check user existence
+        options("/users/:id", (req, res)->{
+            res.type( "application/json");
+
+            int id = Integer.parseInt(req.params(":id"));
+            boolean exists = userService.userExists(id);
+
+            return  gson.toJson("USER EXISTS "+ exists);
+        });
+
+        // DELETE -> Delete an user
+        delete("/users/:id", (req, res)->{
+            res.type( "application/json");
+            int id = Integer.parseInt(req.params(":id"));
+
+            userService.deleteUser(id);
+
+            return gson.toJson("USER DELETED!");
         });
 
         System.out.println("🚀 Spark server running at http://localhost:4567/hello");
---
+++ src/main/java/com/techready/user/UserService.java
@@ -0,0 +1,37 @@
+package com.techready.user;
+
+import java.util.*;
+
+public class UserService {
+    private static final Map<Integer, User> users = new HashMap<>();
+
+    // Constructor adds sample data
+    public UserService() {
+        users.put(1, new User(1, "Alice", "alice@example.com"));
+        users.put(2, new User(2, "Bob", "bob@example.com"));
+    }
+
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    public User getUserById(int id) {
+        return users.get(id);
+    }
+
+    public void addUser(User user) {
+        users.put(user.getId(), user);
+    }
+
+    public void updateUser(int id, User updatedUser) {
+        users.put(id, updatedUser);
+    }
+
+    public void deleteUser(int id) {
+        users.remove(id);
+    }
+
+    public boolean userExists(int id) {
+        return users.containsKey(id);
+    }
+}
